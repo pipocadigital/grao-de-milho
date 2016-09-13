5ff5f7c8b920187c0d3e0641744b823ece2b5a8c
Alison Monteiro <alisonmonteiro.10@gmail.com>
2016-09-13 12:19:20 -0300

    Update the wp-config on helpers.js instead of on wp-install task

--- gulp_tasks/helpers.js
+++ gulp_tasks/helpers.js
@@ -2,94 +2,113 @@ var fs = require('fs');
 var slugify = require('slugify');
 
 function helpers() {
-  function fileExists(filePath) {
-    try {
-      return fs.statSync(filePath).isFile();
-    } catch (err) {
-      return false;
-    }
-  }
+	function fileExists(filePath) {
+		try {
+			return fs.statSync(filePath).isFile();
+		} catch (err) {
+			return false;
+		}
+	}
 
-  function log(message, type) {
-    var date = new Date().toTimeString().split(' ')[0],
-    		dateFormated = '[\x1b[90m' + date + '\x1b[0m] >> ';
-        color = '',
-        log = '';
+	function log(message, type) {
+		var date = new Date().toTimeString().split(' ')[0];
+		var dateFormated = '[\x1b[90m' + date + '\x1b[0m] >> ';
+		var color = '';
+		var log = '';
 
-    switch (type) {
-      case 'danger':
-        color = '\x1b[31m';
-        break;
-      case 'success':
-        color = '\x1b[32m';
-        break;
-      default:
-        color = '\x1b[34m';
-    }
+		switch (type) {
+			case 'danger':
+				color = '\x1b[31m';
+				break;
+			case 'success':
+				color = '\x1b[32m';
+				break;
+			default:
+				color = '\x1b[34m';
+		}
 
-    log += dateFormated + color + message;
-    log += '\x1b[0m';
+		log += dateFormated + color + message;
+		log += '\x1b[0m';
 
-    console.log(log);
-  }
+		console.log(log);
+	}
 
-  function rewriteProjectName(name) {
-	  var bowerJsonUrl = './bower.json';
-	  var packageJsonUrl = './package.json';
-	  var packageJson = require('.' + packageJsonUrl);
-	  var slug = slugify(name).toLowerCase();
-	  var that = this;
+	function rewriteProjectName(name) {
+		var bowerJsonUrl = './bower.json';
+		var packageJsonUrl = './package.json';
+		var packageJson = require('.' + packageJsonUrl);
+		var slug = slugify(name).toLowerCase();
+		var that = this;
 
-	  if (name == '') {
-	  	this.log('Please, give us a project name using the `--p` param.', 'danger');
-	  	process.exit();
-	  }
+		if (name == '') {
+			this.log('Please, give us a project name using the `--p` param.', 'danger');
+			process.exit();
+		}
 
-	  this.log('Config package.json...', 'success');
-	  this.log('Config bower.json...', 'success');
+		this.log('Config package.json...', 'success');
+		this.log('Config bower.json...', 'success');
 
-	  fs.readFile(packageJsonUrl, 'utf8', function(err, data) {
-	  	var versionRegex, newPackageContent;
+		fs.readFile(packageJsonUrl, 'utf8', function (err, data) {
+			var versionRegex, newPackageContent;
 
-	    if (err) {
-	    	console.log(err, 'danger');
-	    	process.exit();
-	    }
+			if (err) {
+				console.log(err, 'danger');
+				process.exit();
+			}
 
-	    newPackageContent = JSON.parse(data);
+			newPackageContent = JSON.parse(data);
 
-	    newPackageContent.version = '0.0.0';
-	    newPackageContent.name = slug;
-	    newPackageContent.title = name;
-	    newPackageContent.description = name;
+			newPackageContent.version = '0.0.0';
+			newPackageContent.name = slug;
+			newPackageContent.title = name;
+			newPackageContent.description = name;
 
-	    writeOn(packageJsonUrl, JSON.stringify(newPackageContent, null, '  '));
-	  });
+			writeOn(packageJsonUrl, JSON.stringify(newPackageContent, null, '  '));
+		});
 
-		fs.readFile(bowerJsonUrl, 'utf8', function(err, data) {
+		fs.readFile(bowerJsonUrl, 'utf8', function (err, data) {
 			var newBowerFile;
 
-		 	if (err) {
-		 		that.log(err, 'danger');
-		 		process.exit();
-		 	}
+			if (err) {
+				that.log(err, 'danger');
+				process.exit();
+			}
 
-		 	writeOn(bowerJsonUrl, data.replace( /grao-de-milho/g, slug));
+			writeOn(bowerJsonUrl, data.replace(/grao-de-milho/g, slug));
 		});
-  }
+	}
 
-  function writeOn(file, content) {
-  	var that = this;
+	function updateWpConfig(dbOptions) {
+		var wpConfigUrl = './wp-config.php';
+		var that = this;
 
-  	fs.writeFile(file, content, 'utf8', function(err) {
-	    if (err) {
-		 		that.log(err, 'danger');
-		 		process.exit();
-		 	}
-	 	});
-  }
+		fs.readFile(wpConfigUrl, 'utf8', function (err, data) {
+			if (err) {
+				that.log(err, 'danger');
+				process.exit(1)
+			}
 
-  return { slugify, fileExists, log, rewriteProjectName };
+			data = data.replace(/database_name_here/g, dbOptions.name);
+			data = data.replace(/username_here/g, dbOptions.user);
+			data = data.replace(/password_here/g, dbOptions.pass);
+			data = data.replace(/host_here/g, dbOptions.host);
+
+			writeOn(wpConfigUrl, data);
+		});
+	}
+
+	function writeOn(file, content) {
+		var that = this;
+
+		fs.writeFile(file, content, 'utf8', function (err) {
+			if (err) {
+				that.log(err, 'danger');
+				process.exit();
+			}
+		});
+	}
+
+	return { slugify, fileExists, log, rewriteProjectName, updateWpConfig };
 };
 
 module.exports = helpers();
