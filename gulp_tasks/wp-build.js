71c38c5f30d3737764c0f962d560b247eadcee5e
Alison Monteiro <alisonmonteiro.10@gmail.com>
2016-09-15 21:19:26 -0300

    Fix indentation and remove unnecessary code and path
    Remove path `plugins/`
    Remove unnecessary code and add a `:` after questions about the database on wp-install task
    Fix indentation on wp-build task

--- gulp_tasks/wp-build.js
+++ gulp_tasks/wp-build.js
@@ -3,27 +3,30 @@ var gulp            = require('gulp'),
 
 // WP Files
 gulp.task('wp-build', function() {
-    gulp.src([gulp.paths.pluginsWp])
-      .pipe(plumber({
-        errorHandler: function(error) {
-          console.log(error.message);
-          this.emit('end');
-    }}))
-    .pipe(gulp.dest('wordpress/wp-content/plugins/'));
+	gulp.src([gulp.paths.pluginsWp])
+		.pipe(plumber({
+			errorHandler: function(error) {
+				console.log(error.message);
+				this.emit('end');
+			}
+		}))
+		.pipe(gulp.dest('wordpress/wp-content/plugins/'));
 
-    gulp.src('wp-config.php')
-    .pipe(plumber({
-      errorHandler: function(error) {
-        console.log(error.message);
-        this.emit('end');
-    }}))
-    .pipe(gulp.dest(gulp.paths.basePath));
+	gulp.src('wp-config.php')
+		.pipe(plumber({
+			errorHandler: function(error) {
+				console.log(error.message);
+				this.emit('end');
+			}
+		}))
+		.pipe(gulp.dest(gulp.paths.basePath));
 
-    gulp.src('src/style.css')
-    .pipe(plumber({
-      errorHandler: function(error) {
-        console.log(error.message);
-        this.emit('end');
-    }}))
-    .pipe(gulp.dest(gulp.paths.pagesDest));
+	gulp.src('src/style.css')
+		.pipe(plumber({
+			errorHandler: function(error) {
+				console.log(error.message);
+				this.emit('end');
+			}
+		}))
+		.pipe(gulp.dest(gulp.paths.pagesDest));
 });
