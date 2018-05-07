695b6f7cbaff964569690e16b59ccac0b534605a
Alison Monteiro <alisonmonteiro.10@gmail.com>
2018-05-07 17:11:35 -0300

    chore(css): task to load some css from `frist view` inline

--- src/header.php
+++ src/header.php
@@ -6,6 +6,8 @@
 		<meta charset="utf-8"/>
 		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 
+		<link inject-first-view-css href="<?php echo get_bloginfo('template_url') ?>/css/first-view.min.css" rel="stylesheet">
+
 		<link href="<?php echo get_bloginfo('template_url') ?>/css/libs.min.css" rel="stylesheet"/>
 		<link href="<?php echo get_bloginfo('template_url') ?>/css/main.min.css" rel="stylesheet"/>
 	</head>
